482dbe209d5b4a443d046cbe016e0ee41e31b51e
Annia <st22295@ormiston.school.nz>
2022-08-28 21:35:37 +1200

    add comments

--- control-structures/easygui/gui_test.py
+++ control-structures/easygui/gui_test.py
@@ -1,3 +1,5 @@
-import easygui
+import easygui  
+# if you encounter this error "ModuleNotFoundError: No module named 'easygui'" this means you need to install it.
+# command : pip install easygui
 
 easygui.enterbox("Hi")
